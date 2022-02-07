256ee41e583902450a57daf3befe7d002e4c2194
Matt Wilson <matt@mttwlsn.com>
2022-02-07 10:48:22 -0500

    Updated site copy.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <div class="p-8">
 	<h1 class="font-extrabold text-4xl">This is the online home of Matt Wilson</h1>
 	<h2 class="text-2xl pt-3 max-w-prose">
-		A quality-obsessed Software Developer who loves getting stuff shipped and making users happy.
+		A quality-obsessed Software Developer zeroed in on getting stuff shipped.
 	</h2>
 </div>
