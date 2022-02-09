8bf8b7dc619435cd114c296fe1423b713724755c
Matt Wilson <matt@mttwlsn.com>
2022-02-09 00:38:53 -0500

    Added splitbee analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	import splitbee from '@splitbee/web';
 	import Header from '$lib/header/header.svelte';
 	import '../app.css';
+
+	splitbee.init({
+		scriptUrl: "/bee.js",
+		apiUrl: "/_hive"
+	});
 </script>
 
 <Header />
