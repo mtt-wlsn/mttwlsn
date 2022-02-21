1b9e699c419aa8fb08297702831938215d026cde
Matt Wilson <matt@mttwlsn.com>
2022-02-20 23:12:06 -0500

    styled the changelog activity box.

--- src/routes/changelog/index.svelte
+++ src/routes/changelog/index.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+    import pullRequestIcon from '$lib/icons/git-pull-request.svg';
+    import activityIcon from '$lib/icons/activity.svg';
+</script>
+
 <h1 class="font-extrabold text-4xl leading-relaxed">Changelog</h1>
 <p class="text-lg pt-6 max-w-prose leading-relaxed">Somewhat based on <a href="https://keepachangelog.com/">"keep a changelog"</a> this page exists as a fun way for me to display a curated list of changes I have made to this website.</p>
+
+<section class="mt-6 p-10 pl-12 border-4 border-slate-800 rounded-lg bg-slate-800 w-fit shadow-lg hover:border-amber-600 transition ease-in">
+    <div class="bg-amber-600 p-4 rounded-full w-fit absolute -ml-20 -mt-3"><img alt="Activity Icon" class="" src="{activityIcon}" /></div>
+    <h2 class="text-2xl font-extrabold max-w-prose">Started Capturing Analytics</h2>
+    <h4 class="text italic pb-3">Feb 9<sup>th</sup>, 2022 • <a class="underline" href="https://github.com/mtt-wlsn/mttwlsn/pull/4">pull request</a> <img alt="Pull Request Icon" class="p-1 inline-block" src="{pullRequestIcon}" /></h4>
+    <p class="text-lg leading-relaxed max-w-prose">Began collecting website analytics using an analytics & conversion platform called <a class="underline" href="https://splitbee.io/">Splitbee</a>.  I wanted to try something other than Google Analytics and Splitbee offered a free tier so I thought I'd try it out.</p>
+</section>
