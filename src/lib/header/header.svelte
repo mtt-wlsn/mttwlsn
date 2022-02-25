d9b466b1c6aa484aa24b386c260a2699bbe38df7
Matt Wilson <matt@mttwlsn.com>
2022-02-24 21:17:01 -0500

    Added theme toggle button.

--- src/lib/header/header.svelte
+++ src/lib/header/header.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Branding from './branding.svelte';
+	import ThemeControls from './theme-controls.svelte';
 </script>
 
-<header class="pt-10">
+<header class="p-8 pt-10 flex justify-between">
 	<Branding />
+	<ThemeControls />
 </header>
