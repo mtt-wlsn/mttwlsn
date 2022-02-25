c769276d0641b8849de52fc33fb6870cf4303918
Matt Wilson <matt@mttwlsn.com>
2022-02-25 07:33:01 -0500

    Removed margin around changelogs.

--- src/routes/changelog/index.svelte
+++ src/routes/changelog/index.svelte
@@ -14,29 +14,27 @@
 	a fun way for me to display a curated list of changes I have made to this website.
 </p>
 
-<div class="ml-10">
-	{#each changelog_activities as activity, i}
-		<section
-			class="mt-7 p-8 pl-11 border-4 rounded-lg w-fit shadow-xl transition ease-in
-				border-gray-300 dark:border-gray-700
-				bg-gray-200 dark:bg-gray-800
-				hover:border-amber-500 dark:hover:border-amber-600
-				hover:bg-gray-100 dark:hover:bg-gray-900">
-			<div class="bg-amber-500 dark:bg-amber-600 p-5 rounded-full w-fit mb-4 -mt-16 -ml-5">
-				<ActivityIcon />
-			</div>
-			<h2 class="text-2xl font-extrabold max-w-prose">{activity.title}</h2>
-			<h4 class="text italic pb-3">
-				{activity.created_at} • <PullRequestIcon /> <a href={activity.pull_request_link}>pull request</a>
-			</h4>
-			<p class="text-lg leading-relaxed max-w-prose">{@html activity.description}</p>
-		</section>
+{#each changelog_activities as activity, i}
+	<section
+		class="mt-7 p-8 pl-11 border-4 rounded-lg w-fit shadow-xl transition ease-in
+			border-gray-300 dark:border-gray-700
+			bg-gray-200 dark:bg-gray-800
+			hover:border-amber-500 dark:hover:border-amber-600
+			hover:bg-gray-100 dark:hover:bg-gray-900">
+		<div class="bg-amber-500 dark:bg-amber-600 p-5 rounded-full w-fit mb-4 -mt-16 -ml-5">
+			<ActivityIcon />
+		</div>
+		<h2 class="text-2xl font-extrabold max-w-prose">{activity.title}</h2>
+		<h4 class="text italic pb-3">
+			{activity.created_at} • <PullRequestIcon /> <a href={activity.pull_request_link}>pull request</a>
+		</h4>
+		<p class="text-lg leading-relaxed max-w-prose">{@html activity.description}</p>
+	</section>
 
-		{#if i + 1 != changelog_activities.length}
-			<div class="divider pt-10 border-l-4 border-gray-400 dark:border-gray-800 border-dashed">&nbsp;</div>
-		{/if}
-	{/each}
-</div>
+	{#if i + 1 != changelog_activities.length}
+		<div class="divider pt-10 border-l-4 border-gray-400 dark:border-gray-800 border-dashed">&nbsp;</div>
+	{/if}
+{/each}
 
 <style lang="scss">
 	.divider {
