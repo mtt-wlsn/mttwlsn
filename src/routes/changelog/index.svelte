e809dab25792a42faff6c680a5f17858b4111f02
Matt Wilson <matt@mttwlsn.com>
2022-02-24 20:46:12 -0500

    Added support for both light and dark mode.

--- src/routes/changelog/index.svelte
+++ src/routes/changelog/index.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import pullRequestIcon from '$lib/icons/git-pull-request.svg';
-	import activityIcon from '$lib/icons/activity.svg';
+	import PullRequestIcon from '$lib/icons/git-pull-request.svelte';
+	import ActivityIcon from '$lib/icons/activity.svelte';
 
 	// I'm just using json data from directly within my source
 	// code.  Someday, I might move this to behind an api,
 	// but for now... why should I do that?
-	import changelog_activities from '../../../src/data/changelog.json';
+	import changelog_activities from '$lib/../data/changelog.json';
 </script>
 
 <h1 class="font-extrabold text-4xl leading-relaxed">Changelog</h1>
@@ -16,15 +16,17 @@
 
 {#each changelog_activities as activity, i}
 	<section
-		class="mt-7 p-8 pl-11 border-4 border-gray-700 rounded-lg bg-gray-800 w-fit shadow-lg hover:border-amber-600 hover:bg-gray-900 transition ease-in"
-	>
-		<div class="bg-amber-600 p-5 rounded-full w-fit mb-4 -mt-16 -ml-5">
-			<img alt="Activity Icon" src={activityIcon} />
+		class="mt-7 p-8 pl-11 border-4 rounded-lg w-fit shadow-xl transition ease-in
+			border-gray-300 dark:border-gray-700
+			bg-gray-200 dark:bg-gray-800
+			hover:border-amber-500 dark:hover:border-amber-600
+			hover:bg-gray-100 dark:hover:bg-gray-900">
+		<div class="bg-amber-500 dark:bg-amber-600 p-5 rounded-full w-fit mb-4 -mt-16 -ml-5">
+			<ActivityIcon />
 		</div>
 		<h2 class="text-2xl font-extrabold max-w-prose">{activity.title}</h2>
 		<h4 class="text italic pb-3">
-			{activity.created_at} • <a href={activity.pull_request_link}>pull request</a>
-			<img alt="Pull Request Icon" class="p-1 inline-block" src={pullRequestIcon} />
+			{activity.created_at} • <PullRequestIcon /> <a href={activity.pull_request_link}>pull request</a>
 		</h4>
 		<p class="text-lg leading-relaxed max-w-prose">{@html activity.description}</p>
 	</section>
